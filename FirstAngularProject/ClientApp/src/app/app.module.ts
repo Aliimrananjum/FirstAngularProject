a783a42b0d516a6df376901c6f33f8c85353d035
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2023-11-05 20:38:12 +0100

    Added items (empty)

--- FirstAngularProject/ClientApp/src/app/app.module.ts
+++ FirstAngularProject/ClientApp/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
+    ItemsComponent
 
   ],
   imports: [
@@ -22,6 +24,7 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'items', component: ItemsComponent },
 
     ])
   ],
