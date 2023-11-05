31c6e7938d7282c46d3020e727201dad1e8b8495
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2023-11-05 21:00:27 +0100

    Added some items and now visable in items page

--- FirstAngularProject/ClientApp/src/app/items/items.component.ts
+++ FirstAngularProject/ClientApp/src/app/items/items.component.ts
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
 })
 
 export class ItemsComponent {
+  displayImage: boolean = true;
+  viewTitle: string = 'Table';
+  items: any[] = [
+    {
+      "ItemId": 1,
+      "Name": "Pizza",
+      "Price": 150,
+      "Description": "Delicious Italian dish with a thin crust topped with tomato sauce, cheese, and various toppings.",
+      "ImageUrl": "assets/images/pizza.jpg"
+    },
+    {
+      "ItemId": 2,
+      "Name": "Fried Chicken Leg",
+      "Price": 20,
+      "Description": "Crispy and succulent chicken leg that is deep-fried to perfection, often served as a popular fast food item.",
+      "ImageUrl": "assets/images/chickenleg.jpg"
+    }
+  ];
+
+  toggleImage(): void {
+    this.displayImage = !this.displayImage;
+  }
+
 }
