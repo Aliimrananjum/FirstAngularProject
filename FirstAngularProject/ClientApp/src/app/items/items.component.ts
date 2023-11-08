b0176f9522e18943380656a39f631d64bf5db2f1
Ali Imran Anjum <89576402+Aliimrananjum@users.noreply.github.com>
2023-11-08 15:06:13 +0100

    Now able to CallToServer through API and converting data from server to JSON to display on frontend

--- FirstAngularProject/ClientApp/src/app/items/items.component.ts
+++ FirstAngularProject/ClientApp/src/app/items/items.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {IItem } from './item'
+import { Component, OnInit } from '@angular/core';
+import { IItem } from './item'
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-items-component',
@@ -7,9 +8,38 @@ import {IItem } from './item'
   styleUrls:['./items.component.css']
 })
 
-export class ItemsComponent {
+export class ItemsComponent implements OnInit{
   displayImage: boolean = true;
   viewTitle: string = 'Table';
+  items: IItem[] = [];
+
+  constructor(private _http: HttpClient) {
+
+  }
+  //listFilter: string = '';
+
+  private _listFilter: string = '';
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter', value);
+    this.filteredItems = this.performFilter(value)
+  }
+
+  getItems(): void {
+    this._http.get<IItem[]>("api/item/").subscribe(data => {
+      console.log('All', JSON.stringify(data));
+      this.items = data;
+      this.filteredItems = this.items;
+    })
+  }
+
+
+  /*
   items: IItem[] = [
     {
       "ItemId": 1,
@@ -26,9 +56,22 @@ export class ItemsComponent {
       "ImageUrl": "assets/images/chickenleg.jpg"
     }
   ];
+   */
+
+  filteredItems: IItem[] = this.items;
+
+  performFilter(filterBy: string): IItem[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.items.filter((item: IItem) => item.Name.toLocaleLowerCase().includes(filterBy));
+  }
 
   toggleImage(): void {
     this.displayImage = !this.displayImage;
   }
+
+  ngOnInit(): void {
+    console.log('ItemsComponent created')
+  }
+ 
 
 }
